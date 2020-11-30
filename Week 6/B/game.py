fe9f0ab0d9097b65159c37e69d0158507652ce75
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-11-30 14:11:47 -0500

    B6: add loads of comments, fix hitboxes and other changes
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- Week 6/B/game.py
+++ Week 6/B/game.py
@@ -5,31 +5,33 @@ from pygame.locals import *
 import random
 import time
 
-# Initializing
+# Init pygame
 pygame.init()
 
-# Setting up FPS
+# SetFPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-# Creating colors
+# Colours ( we didn't actually need all of them Max :) )
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Other Variables for use in the program
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 900
-SPEED = 5
+# Settings ( I made this for a 4k monitor btw,
+#            so change this if your screen is too small :D )
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1600
+SPEED = 7
 SCORE = 0
 
-# Setting up Fonts
+# Fonts
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 
+# Background Image
 background = pygame.image.load("bg.jpg")
 
 # Create a white screen
@@ -37,44 +39,66 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+### CLASES ###
+# ** these classes describe how they will behave each time
+# ** a new "Sprite" (pygame term) is created
+# Enemy
+
 
 class Enemy(pygame.sprite.Sprite):
+    # NOTE: all objects that are created (including this "Sprite") runs the
+    # `__init__` function first (on initialization)!
     def __init__(self):
-        super().__init__()
+        super().__init__()  # call pygame's Sprite super class's `__init__` function
+        # Image of the missile
         self.image = pygame.image.load("Enemy.png")
-        self.surf = pygame.Surface((42, 70))
-        self.rect = self.surf.get_rect(
-            center=(random.randint(40, SCREEN_WIDTH-40), 0))
+        # Hitbox of the missile
+        self.surf = pygame.Surface((34, 128))
+        # Spawn location of the missile, 20 is the minimum distance from either side of the SCREEN
+        self.rect = self.surf.get_rect(  # the `randint` function returns a random
+            # number between the two inputted values
+            center=(random.randint(20, SCREEN_WIDTH-20), 0))
 
     def move(self):
-        global SCORE
+        global SCORE  # recall that we learnt that you must use the `global` keyword
+        # if you want to use a "global" variable!
         self.rect.move_ip(0, SPEED)
-        if (self.rect.top > 600):
+        # keep moving missile downwards until it nearly reaches the bottom of the screen
+        if (self.rect.top > SCREEN_HEIGHT-10):
             SCORE += 1
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
+# Player (fighter jet)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
+        super().__init__()  # same as enemy
+        # Load image of the fighter jet
         self.image = pygame.image.load("Player.png")
-        self.surf = pygame.Surface((40, 75))
-        self.rect = self.surf.get_rect(center=(160, 520))
+        # Hitbox
+        self.surf = pygame.Surface((220, 250))
+        # Spawn location of the player sprite
+        self.rect = self.surf.get_rect(
+            center=(SCREEN_WIDTH/2, SCREEN_HEIGHT-170))
 
     def move(self):
         pressed_keys = pygame.key.get_pressed()
-       # if pressed_keys[K_UP]:
-        #self.rect.move_ip(0, -5)
-       # if pressed_keys[K_DOWN]:
-        # self.rect.move_ip(0,5)
-
+        # if pressed_keys[K_UP]:
+        #     self.rect.move_ip(0, -5)
+        # if pressed_keys[K_DOWN]:
+        #     self.rect.move_ip(0,5)
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
         if self.rect.right < SCREEN_WIDTH:
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
+
+######################################
+### THE GAME ACTUALLY STARTS HERE! ###
+######################################
 
 
 # Setting up Sprites
@@ -92,9 +116,8 @@ all_sprites.add(E1)
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
-# Game Loop
+# "Game Loop"
 while True:
-
     # Cycles through all events occurring
     for event in pygame.event.get():
         if event.type == INC_SPEED:
@@ -107,19 +130,21 @@ while True:
     scores = font_small.render(str(SCORE), True, BLACK)
     DISPLAYSURF.blit(scores, (10, 10))
 
-    # Moves and Re-draws all Sprites
+    # Moves and re-renders all the Sprites
     for entity in all_sprites:
         DISPLAYSURF.blit(entity.image, entity.rect)
         entity.move()
 
-    # To be run if collision occurs between Player and Enemy
+    # If a missile hits the fighter jet
     if pygame.sprite.spritecollideany(P1, enemies):
         #   pygame.mixer.Sound('crash.wav').play()
         time.sleep(0.5)
 
+        # Display game over screen
         DISPLAYSURF.fill(RED)
         DISPLAYSURF.blit(game_over, (30, 250))
 
+        # Clean up the game so that there are no memory leaks
         pygame.display.update()
         for entity in all_sprites:
             entity.kill()
@@ -127,5 +152,6 @@ while True:
         pygame.quit()
         sys.exit()
 
+    # Call this stuff after each frame is rendered
     pygame.display.update()
     FramePerSec.tick(FPS)

49902455e05e6c79ab5de28dcbde903963f6058e
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-01-26 10:19:56 -0500

    finish join and leave commands
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- Week 12/B/commands.js
+++ Week 12/B/commands.js
@@ -31,6 +31,7 @@ function init() {
     registerCommand('help')
     registerCommand('suggest')
     registerCommand('join')
+    registerCommand('leave')
     console.log(commandMap)
     console.log(helpMap)
 }
---
+++ Week 12/B/commands/leave.js
@@ -0,0 +1,42 @@
+const Command = require('./command')
+const Roles = require('../utils/roles')
+const config = require('../config.json')
+
+class LeaveCommand extends Command {
+    getAliases() {
+        return ['leave'];
+    }
+
+    async exec(msg, args) {
+        const group = args[0]
+        switch (group) {
+            case 'a':
+            case 'b':
+            case 'A':
+            case 'B':
+                const member = msg.member
+                const roleID = config.roles[group]
+                if (!Roles.has(member, roleID))
+                    msg.reply(`You **already** *left* __**Group ${group.toUpperCase()}**__`), msg.react('👎');
+                else {
+                    const res = Roles.remove(member, roleID)
+                    if (res) {
+                        msg.react('👍')
+                        msg.reply(`You have been *removed* from **Group ${group.toUpperCase()}** role`)
+                        console.log(`${msg.member.tag} was removed the Group ${group.toUpperCase()} role`)
+                    } else { return false }
+                }
+                break
+            default:
+                msg.reply('Invalid Group')
+                return false;
+        }
+        return true;
+    }
+
+    getHelp() {
+        return 'Leave the notifications for a certain group';
+    }
+}
+
+module.exports = LeaveCommand 
